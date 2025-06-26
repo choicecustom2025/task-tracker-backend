2b7d8bc861c6fe9fb62ddd1de52fa1f828b2a4e2
choicecustom2025 <leon@cabinetsbycw.com>
2025-06-26 10:14:54 -0400

    Add files via upload

--- server/index.js
+++ server/index.js
@@ -15,7 +15,9 @@ app.use(express.json());
 app.use('/api/auth', authRoutes);
 app.use('/api/tasks', taskRoutes);
 
-mongoose.connect(process.env.MONGO_URI)
-
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
   .then(() => app.listen(process.env.PORT || 5000, () => console.log('✅ Server running on port', process.env.PORT || 5000)))
   .catch(err => console.error('❌ MongoDB connection error:', err));
